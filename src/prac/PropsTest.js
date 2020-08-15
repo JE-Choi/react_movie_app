54be81871683cda945d95e6551532f2cef6dab8c
최지은 <lielieing@naver.com>
2020-08-15 13:22:08 +0900

    component에 props 전달

---
+++ src/prac/PropsTest.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// props로 인자 받아서 => props.속성 으로 받아도 되긴 함.
+function Food({food}){
+    return(
+    <div>나는 {food}을 좋아해요~</div>
+    );
+}
+function PropsTest() {
+  return (
+      <>
+        <Food food="베이컨"/>
+        <Food food="스파게티"/>
+        <Food food="맛있는 음식"/>
+        <br/><br/>
+        <div>맛있겠죠?</div>
+      </>
+  );
+}
+
+export default PropsTest;
